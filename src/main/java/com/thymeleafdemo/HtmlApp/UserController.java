a7404975915d2a4a85f12a0f3af7170b11deba1d
Hitesh Anney <Hiteshk813@gmail.com>
2023-01-04 23:21:33 +0530

    Simple webapp

---
+++ src/main/java/com/thymeleafdemo/HtmlApp/UserController.java
@@ -0,0 +1,28 @@
+package com.thymeleafdemo.HtmlApp;
+
+import com.thymeleafdemo.HtmlApp.Model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class UserController {
+
+
+        @RequestMapping("/")
+        public String index()
+        {
+            return"index";
+        }
+        @RequestMapping(value="/save", method= RequestMethod.POST)
+        public ModelAndView save(@ModelAttribute User user)
+        {
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setViewName("user-data");
+            modelAndView.addObject("user", user);
+            return modelAndView;
+        }
+    }
